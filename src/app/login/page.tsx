ceef6c2850aa9f58159a9c02ed70c5dddcee2637
Kajee2002 <kajatheepan0311@gmail.com>
2025-03-09 17:46:42 +0530

    fixed: responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,20 +6,20 @@ import Link from "next/link"
 export default function signup(){
     return(
         <>
-        <div className="flex flex-row h-screen width-screen min-w-screen">
-            <div className="flex flex-col  justify-center items-center signup-text w-1/2 py-16 px-4">
-                <h1 className="pb-4 text-6xl">Welcome</h1>
-                <p className="py-4 ">If you already have an account </p>
-                <Button ><Loader2 className="animate-spin hidden" /> <Link href="/sign-up">SignUp Now</Link></Button>
+        <div className="flex flex-col md:flex-row  h-screen w-screen min-w-screen overflow-scroll">
+            <div className="flex flex-col justify-center items-center signup-text h-2/4 md:h-screen w-full md:w-1/2 py-8 px-4 bg-gradient-to-l from-rose-500 via-pink-500 to-red-500 animate-gradient">
+            <h1 className="text-6xl md:text-8xl text-gray-50 font-black drop-shadow-lg">Welcome</h1>
+            <p className="pb-3 pt-2 md:pt-16 text-xl md:text-2xl">If you already have an account</p>
+            <Button className="text-xl md:text-2xl font-semibold my-5 p-4 md:p-6"><Loader2 className="animate-spin hidden" /> <Link href="/sign-up">SignUp Now</Link></Button>
             </div>
-            <div className="flex flex-col justify-center items-center signup-text w-1/2 py-16 px-4">
-                <p className="pb-2 ">Login to your Account </p>
-                <form className="w-3/4  px-4 ">
-                    <Input id="username" type="text" placeholder="UserName" required />
-                    <Input id="password" type="password" placeholder="Password" required />
-                    <Input id="confirmPassword" type="password" placeholder="Confirm Password" required />
-                    <Button type="submit" variant={"outline"} className="">Login <ChevronRight /></Button>
-                </form>
+            <div className="flex flex-col justify-center items-center signup-text h-2/4 mt-2 md:mt-0 md:h-screen w-full md:w-1/2 py-8 md:py-16 px-4 bg-gradient-to-r from-pink-100 to-gray-50 ">
+            <p className="pb-2 text-xl md:text-xl">Login to your Account</p>
+            <form className="w-full md:w-3/4 px-4">
+                <Input id="username" type="text" placeholder="UserName" required className="mb-4" />
+                <Input id="password" type="password" placeholder="Password" required className="mb-4" />
+                <Input id="confirmPassword" type="password" placeholder="Confirm Password" required className="mb-4" />
+                <Button type="submit" variant={"outline"} className="w-full">Login <ChevronRight /></Button>
+            </form>
             </div>
         </div>
         </>
